7698d0a985934aba5ef0f68a4a46eab3c1656d03
Shana Wiltshire <shanawiltshire@gmail.com>
2019-11-17 23:10:53 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,6 +1,6 @@
-cust_age = [37, 31, 41, 52, 16, 5]
+sort_array_asc = [37, 31, 41, 52, 16, 5]
  
-cust_age.sort do |num1, num2|
+ sort_array_asc.sort do |num1, num2|
   if num1 == num2
     0
   elsif num1 < num2
