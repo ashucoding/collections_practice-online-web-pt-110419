bff8209fd04920bc794da50f5d339670b06978d7
Shana Wiltshire <shanawiltshire@gmail.com>
2019-11-17 23:12:40 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,12 +1,6 @@
 sort_array_asc = [37, 31, 41, 52, 16, 5]
  
- sort_array_asc.sort do |num1, num2|
-  if num1 == num2
-    0
-  elsif num1 < num2
-    -1
-  elsif num1 > num2
-    1
-  end
+ def sort_array_asc(array)
+  array.sort
 end
 
