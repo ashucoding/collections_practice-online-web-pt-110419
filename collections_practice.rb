d12bafe9a500bd54a8f71eb91a542fa71a611551
Shana Wiltshire <shanawiltshire@gmail.com>
2019-11-17 23:25:34 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -40,3 +40,12 @@ def kesha_maker(array)
     item[2] = "$"
   end
 
+find_a = ["Africa", "Apple", "Apricot","Amy"]
+def find_a(array)
+  array.find_all do |word|
+    word[0] == "a"
+  end
+  
+  
+  def sum_array
+    array.inject do |
