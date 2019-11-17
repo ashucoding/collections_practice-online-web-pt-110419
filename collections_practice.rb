661308c65bdd03adbfda7cace2c2962c234308eb
Shana Wiltshire <shanawiltshire@gmail.com>
2019-11-17 23:09:24 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,9 +3,9 @@ cust_age = [37, 31, 41, 52, 16, 5]
 cust_age.sort do |num1, num2|
   if num1 == num2
     0
-  elsif a < b
+  elsif num1 < num2
     -1
-  elsif a > b
+  elsif num1 > num2
     1
   end
 end
