b33fb68aa2430544baf4f14ee803f51a60c2235b
Shana Wiltshire <shanawiltshire@gmail.com>
2019-11-17 23:15:23 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -4,9 +4,18 @@ sort_array_asc = [37, 31, 41, 52, 16, 5]
   array.sort
 end
 
+sort_array_desc = [37, 31, 41, 52, 16, 5]
+
 def sort_array_desc(array)
   array.sort do | left, right|
     right <=> left
   end
 end
 
+sort_array_char_count = []
+
+def sort_array_char_count(array)
+  array.sort do |left, right|
+    left.length <=> right.length
+  end
+end
