f0dd92130a38e02672e999202dc448661f9d8148
Shana Wiltshire <shanawiltshire@gmail.com>
2019-11-17 23:19:23 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -25,6 +25,8 @@ swap_elements = ["Mary","John","Andy"]
 def swap_elements(array)
   array[1], array[2] = array[2], array[1]
   array
-  
+end
+
+reverse_array = ["Africa","France","Japan"]
   
 end
